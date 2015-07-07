809c7ce08ba6d19501bbc281ebdd91686a968cac
wataash <expllion@gmail.com>
2015-07-07 11:27:05 +0900

    Developing.

--- Agilent4156C/Program.cs
+++ Agilent4156C/Program.cs
@@ -9,16 +9,10 @@ namespace Agilent4156C
 {
     public static class Program
     {
-        /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
-    //        System.IO.File.WriteAllText(
-    //System.Environment.ExpandEnvironmentVariables("%temp%") +
-    //"\\" + "test.ttxt", "aaa"
-    //);
             string VISAResource = "GPIB0::18::INSTR";
             // Agilent
             var RM = new ResourceManager();
@@ -41,22 +35,26 @@ namespace Agilent4156C
             //Application.Run(new Form1());
         }
 
-        public static List<string> SweepMeasurement(FormattedIO488 DMM, double endV,
-          double stepV, double compI, int groundSMU, int sweepSMU)
+
+        public static void SweepMeasurement(FormattedIO488 DMM, double endV,
+            double stepV, double compI, int groundSMU, int sweepSMU)
         {
             // Hard code
+            string VStr, IStr, filePath, writeStr;
+            double[][] VI;
+            bool abort;
             var timeStamps = new List<string>();
             var voltages = new List<string>();
             var currents = new List<string>();
             DMM.WriteString("*RST");
 
-            // Channel settings
+            // Channel settings ////////////////////////////////////////////////
             // TODO: Integration time, Hold time, Deley time
             // Disable all units
-            foreach (string s in new[] { "SMU1", "SMU2", "SMU3", "SMU4",
+            foreach (string unit in new[] { "SMU1", "SMU2", "SMU3", "SMU4",
                                          "VSU1", "VSU2", "VMU1", "VMU2" })
             {
-                DMM.WriteString(":PAGE:CHAN:" + s + ":DIS");
+                DMM.WriteString(":PAGE:CHAN:" + unit + ":DIS");
             }
             DMM.WriteString(
                 ":PAGE:CHAN:SMU" + groundSMU + ":VNAM 'V" + groundSMU + "';" +
@@ -66,47 +64,94 @@ namespace Agilent4156C
                 ":PAGE:CHAN:SMU" + sweepSMU + ":INAM 'I" + sweepSMU + "';" +
                 ":PAGE:CHAN:SMU" + sweepSMU + ":MODE V;FUNC VAR1;");
 
-            // Measurement setup
+            // Measurement setup ///////////////////////////////////////////////
             DMM.WriteString(":PAGE:MEAS:SWE:VAR1:STAR 0");
             DMM.WriteString(":PAGE:MEAS:VAR1:STEP " + stepV + ";");
             DMM.WriteString(":PAGE:MEAS:VAR1:MODE DOUB;");
-            DMM.WriteString(":PAGE:DISP:SET:GRAP:Y1:MIN " + -1e-12); // TODO: move to other code
-            DMM.WriteString(":PAGE:DISP:SET:GRAP:Y1:MAX " + 1e-12); // TODO: move to other code
+            // TODO: move to another place
+            DMM.WriteString(":PAGE:DISP:SET:GRAP:Y1:MIN " + -1e-12);
+            DMM.WriteString(":PAGE:DISP:SET:GRAP:Y1:MAX " + 1e-12);
 
-            //foreach (var item in collection)
             foreach (double v in AlternativeRange(0.1, 0.1, endV)) // 100mV step
             {
-                // Measure setup
+                // Measure setup ///////////////////////////////////////////////
                 DMM.WriteString(":PAGE:DISP:SET:GRAP:X:MIN " + -Math.Abs(v));
                 DMM.WriteString(":PAGE:DISP:SET:GRAP:X:MAX " + Math.Abs(v));
-                // Measure
+                // Measure /////////////////////////////////////////////////////
                 DMM.WriteString(":PAGE:MEAS:SWE:VAR1:STOP " + v);
-                string initTime = DateTime.Now.ToString(); // 2015/07/06 20:13:08
-                initTime = initTime.Replace(":", "-");
-                initTime = initTime.Replace("/", "-");
-                initTime = initTime.Replace(" ", "_"); // 2015-07-06_20-13-08
+                string initTime = GetTime(); // 2015/07/06 20:13:08
                 DMM.WriteString(":PAGE:SCON:MEAS:APP");
                 //DMM.WriteString(":PAGE:SCON:MEAS:SING");
                 //DMM.WriteString(":PAGE:SCON:MEAS:STOP");
                 DMM.WriteString("*OPC?");
                 DMM.ReadString();
+                // Acuire and save data ////////////////////////////////////////
                 //DMM.WriteString(":FORM:BORD NORM;DATA REAL, 64;:DATA? 'V2';");
                 DMM.WriteString(":FORM:DATA ASC;:DATA? 'V" + sweepSMU + "';");
-                voltages.Add(DMM.ReadString());
+                //voltages.Add(DMM.ReadString());
+                VStr = DMM.ReadString();
                 //DMM.WriteString(":FORM:BORD NORM;DATA REAL, 64;:DATA? 'I2';");
                 DMM.WriteString(":FORM:DATA ASC;:DATA? 'I" + sweepSMU + "';");
-                currents.Add(DMM.ReadString());
-                // break if (V/deltaV + 1 != points);
-                System.IO.File.WriteAllText(
-                    System.Environment.ExpandEnvironmentVariables("%temp%") +
-                    "\\" + initTime + ".txt",
-                    currents.Last());
+                //currents.Add(DMM.ReadString());
+                IStr = DMM.ReadString();
 
-                //System.IO.File.Write
+                abort = ZipDetectInf(CommaStringToDoubleArray(VStr),
+                    CommaStringToDoubleArray(IStr), out VI);
+                writeStr = TwoDimDouble2String(VI);
+                writeStr = initTime + "\nV,I\n" + writeStr;
+                filePath = Environment.ExpandEnvironmentVariables("%temp%") +
+                    "\\" + initTime + ".txt";
+                System.IO.File.WriteAllText(filePath, writeStr);
+                if (abort) break; // Finish if "stop button" on 4156C pressed.
             }
-            return voltages;
+            return;
         }
 
+        /// <summary>
+        /// Example: 
+        /// Write2DimDouble(
+        ///     new[] { new[] { 1.0, 2, }, new[] { 3.0, 4 } });
+        /// returns "1,2\n3,4"
+        /// </summary>
+        /// <param name="data"></param>
+        public static string TwoDimDouble2String(double[][] data)
+        {
+            string joined = "";
+            foreach (double[] item in data)
+            {
+                joined += String.Join(",", item) + "\n";
+            }
+            return joined;
+        }
+
+        /// <summary>
+        /// Verified only in Japanese locale.
+        /// </summary>
+        /// <returns>Example: "2015-07-06_20-13-08"</returns>
+        public static string GetTime()
+        {
+            string t = DateTime.Now.ToString();
+            // 2015/07/06 20:13:08 --> 2015-07-06_20-13-08
+            return t.Replace(":", "-").Replace("/", "-").Replace(" ", "_");
+        }
+        /// <summary>
+        /// CommaStringToDoubleArray("1,2,3e3") --> {1.0, 2.0, 3000.0}
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static double[] CommaStringToDoubleArray(string s)
+        {
+            return Array.ConvertAll(s.Split(','), Double.Parse);
+        }
+
+        /// <summary>
+        /// Example: AlternativeRange(1.00, 0.11, 1.55)
+        /// returns {1.00, -1.00, 1.11, -1.11, 1.22, -1.22}
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="delta"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         public static double[] AlternativeRange(double start, double delta, double end)
         {
             // TODO: smart way using LINQ?
@@ -121,5 +166,31 @@ namespace Agilent4156C
             return res.ToArray();
         }
 
+        /// <summary>
+        /// Grater than 1e10: Infinite.
+        /// Example: 
+        /// double[][] o;
+        /// ZipDetectInf(new[] { 1, 1e20, 3 }, new[] { 1.1, 3.3, 800 }, out o);
+        /// returns true (1e20 > 1e10), o = new[][] {{1.0, 1.1}, {3.0, 800.0}}
+        /// </summary>
+        /// <param name="in1"></param>
+        /// <param name="in2"></param>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static bool ZipDetectInf(double[] in1, double[] in2, out double[][] o)
+        {
+            bool detectInfinity = false;
+            IEnumerable<double[]> zipped = in1.Zip(in2, (a, b) => new[] { a, b });
+            var zipInfRemoved = new List<double[]>();
+            foreach (double[] pair in zipped)
+            {
+                if (pair[0] > 1e10 || pair[1] > 1e10)
+                    detectInfinity = true;
+                else
+                    zipInfRemoved.Add(pair);
+            }
+            o = zipInfRemoved.ToArray();
+            return detectInfinity;
+        }
     }
 }
