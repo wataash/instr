d2aff40132e04699174a4e75214efeb0e51d231e
wataash <expllion@gmail.com>
2015-07-12 17:23:20 +0900

    Make channel setting function.

--- Agilent4156C/Agilent4156C.cs
+++ Agilent4156C/Agilent4156C.cs
@@ -1,34 +1,20 @@
 ﻿using System;
+using System.Linq;
 using static Instr.Functions;
 
 namespace Instr
 {
     public static class Agilent4156C
     {
-        private static string ErrorQ(IVisaCommunicator visa)
-        {
-            return visa.Query("SYST:ERR?");
-        }
-
         public static void ContactTest(IVisaCommunicator visa, double timeInterval,
             double timeEnd)
         {
             visa.Write("*RST");
             visa.Write(":PAGE:CHAN:MODE SAMP;"); // not in GPIB mannual damn
 
-            foreach (string unit in new[] { "SMU1", "SMU2", "SMU3", "SMU4",
-                                         "VSU1", "VSU2", "VMU1", "VMU2" })
-            {
-                visa.Write(":PAGE:CHAN:" + unit + ":DIS");
-            }
-            visa.Write(
-                ":PAGE:CHAN:SMU1:VNAM 'V1';" +
-                ":PAGE:CHAN:SMU1:INAM 'I1';" +
-                ":PAGE:CHAN:SMU1:MODE COMM;FUNC CONS;" +
-                ":PAGE:CHAN:SMU3:VNAM 'V3';" +
-                ":PAGE:CHAN:SMU3:INAM 'I3';" +
-                ":PAGE:CHAN:SMU3:MODE V;FUNC CONS;"
-                );
+            DisableAllUnits(visa, false, 1, 3);
+            ConfugureSmu(visa, false, 1, 3, 3);
+            ConfugureSmu(visa, false, 3, 1, 3);
 
             visa.Write(":PAGE:MEAS:SAMP:IINT 50e-3;POIN 1201;");
             visa.Write(":PAGE:MEAS:SAMP:CONS:SMU3 1e-3;");
@@ -77,19 +63,9 @@ namespace Instr
 
             // Channel settings ////////////////////////////////////////////////
             // TODO: Integration time, Hold time, Deley time
-            // Disable all units
-            foreach (string unit in new[] { "SMU1", "SMU2", "SMU3", "SMU4",
-                                         "VSU1", "VSU2", "VMU1", "VMU2" })
-            {
-                visa.Write(":PAGE:CHAN:" + unit + ":DIS");
-            }
-            visa.Write(
-                ":PAGE:CHAN:SMU" + groundSMU + ":VNAM 'V" + groundSMU + "';" +
-                ":PAGE:CHAN:SMU" + groundSMU + ":INAM 'I" + groundSMU + "';" +
-                ":PAGE:CHAN:SMU" + groundSMU + ":MODE COMM;FUNC CONS;" +
-                ":PAGE:CHAN:SMU" + sweepSMU + ":VNAM 'V" + sweepSMU + "';" +
-                ":PAGE:CHAN:SMU" + sweepSMU + ":INAM 'I" + sweepSMU + "';" +
-                ":PAGE:CHAN:SMU" + sweepSMU + ":MODE V;FUNC VAR1;");
+            DisableAllUnits(visa, false, groundSMU, sweepSMU);
+            ConfugureSmu(visa, false, groundSMU, 3, 3);
+            ConfugureSmu(visa, false, sweepSMU, 1, 1);
 
             // Measurement setup ///////////////////////////////////////////////
             visa.Write(":PAGE:MEAS:SWE:VAR1:STAR 0");
@@ -135,24 +111,85 @@ namespace Instr
                 System.IO.File.WriteAllText(filePath, writeStr);
                 if (abort) break; // Finish if "stop button" on 4156C pressed.
             }
+            DisableAllUnits(visa, false, 1, 2);
             return;
         }
 
+
+
         /// <summary>
-        /// 
+        /// Get a response of "SYST:ERR?" command.
         /// </summary>
-        /// <param name="channels">
-        /// 1: SMU1, 2: SMU2, 3: SMU3, 4: SMU4, 5: VSU1, 6: VSU2, 7: VMU1, 8: VMU2
-        /// </param>
+        /// <param name="visa"></param>
+        /// <param name="useUSCommands"></param>
         /// <returns></returns>
-        private static int ChannelSetup(params int[] channels)
+        private static string ErrorQ(IVisaCommunicator visa, bool useUSCommands)
         {
+            if (useUSCommands)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                return visa.Query("SYST:ERR?");
+            }
+        }
 
-            // if elem in channels
-            // TODO: enum, ChannelSetup in another proj.
-            throw new NotImplementedException();
-            return 0;
+        /// <summary>
+        /// Disable all units. if parameter "exceptUnits" is given, corresponding units
+        /// (1: SMU1, 2: SMU2, 3: SMU3, 4: SMU4, 5: VSU1, 6: VSU2, 7: VMU1, 8: VMU2)
+        /// are not disabled.
+        /// </summary>
+        /// <param name="visa"></param>
+        /// <param name="useUSCommands"></param>
+        /// <param name="exceptUnits">Numbers less than 1 or grater than 8 are ignored.</param>
+        /// <example>
+        /// DisableAllUnits(visa, false, 1, 3, 6) disables SMU1, SMU2, SMU4, VSU1, VMU1 and VMU2.
+        /// </example>
+        private static void DisableAllUnits(IVisaCommunicator visa, bool useUSCommands, params int[] exceptUnits)
+        {
+            if (useUSCommands)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                string[] units = new[] { "SMU1", "SMU2", "SMU3", "SMU4", "VSU1", "VSU2", "VMU1", "VMU2" };
+                for (int i = 1; i <= 8; i++)
+                {
+                    if (exceptUnits.Contains(i)) continue;
+                    visa.Write(":PAGE:CHAN:" + units[i] + ":DIS");
+                }
+            }
+        }
 
+        /// <summary>
+        /// Implimenting.
+        /// </summary>
+        /// <param name="visa"></param>
+        /// <param name="useUSCommands"></param>
+        /// <param name="SmuNumber">1, 2, 3, or 4</param>
+        /// <param name="mode">1: V, 2: I, 3: common</param>
+        /// <param name="func">1: VAR1, 2: VAR2, 3: CONSTANT, 4: VAR1'</param>
+        /// <param name="VName">default: "V#SMU"</param>
+        /// <param name="IName">default: "I#SMU"</param>
+        private static void ConfugureSmu(IVisaCommunicator visa, bool useUSCommands,
+            int SmuNumber, int mode, int func, string VName = null, string IName = null)
+        {
+            if (VName == null) VName = "V" + SmuNumber;
+            if (IName == null) IName = "I" + SmuNumber;
+            if (useUSCommands)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                if (mode == 0 | func == 0) throw new ArgumentOutOfRangeException(); // if negative value or larger value,
+                string modeStr = new string[] { null, "V", "I", "COMM" }[mode]; // these code will throw exceptions.
+                string funcStr = new string[] { null, "VAR1", "VAR2", "CONS", "VARD" }[func]; // (not tested)
+                string writeStr = $":PAGE:CHAN:SMU{SmuNumber}:VNAM '{VName}';INAM '{IName}';MODE {modeStr};FUNC {funcStr};";
+                visa.Write(writeStr);
+            }
         }
 
     }
