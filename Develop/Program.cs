f9589e18fc745360bbd599b90127a8daa561e7b7
wataash <expllion@gmail.com>
2015-07-14 15:49:59 +0900

    List commands of ProberBench

--- Develop/Program.cs
+++ Develop/Program.cs
@@ -4,10 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Instr;
 using static Instr.Functions;
 
-namespace Develop
+namespace Instr.Develop
 {
     class Program
     {
--- Instr/SussPA300.cs
+++ Instr/SussPA300.cs
@@ -9,8 +9,6 @@ namespace Instr
         // - GPIB Interface
         // - Kernel Commands
         // 
-        // *IDN?
-        // 
         // ReadChuckPosition
         // ReadChuck Heights
         // MoveChuckSeparation
@@ -20,6 +18,26 @@ namespace Instr
         public SussPA300(string visaResource) : base(visaResource)
         {
             return;
+        }
+
+        void Test()
+        {
+            // near home position!
+
+            // -- read --
+            Query("ReadChuckPosition Y H D"); // home, separate: "0: 0.0 -0.5 -300.08"
+                                              // home, align:    "0: 0.0 0.5 -99.95"
+                                              // home, cont:     "0: 0.0 0.5 -1.27e-02"
+            Query("ReadChuckIndex"); // 0: 1300.0 1300.0
+
+            // -- move --
+            Query("MoveChuckSeparation 5"); // 5% velocity
+            Query("MoveChuckAlign 5"); // 5% velocity
+            Query("MoveChuckContact 5"); // 5% velocity
+            Query("MoveChuckIndex 0 0 H 5"); // from home position, 5% velocity
+
+            // -- etc --
+            Query("StopChuckMovement 7");
         }
 
     }
