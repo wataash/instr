a023d52e3e280059bf1929f0bcd069accb06c9ac
wataash <expllion@gmail.com>
2015-08-03 07:19:52 +0900

    Modify

--- test.py
+++ test.py
@@ -17,25 +17,29 @@ suss_visa_timeout_sec = 10
 # Directory
 datadir = os.environ['appdata'] + r'\Instr\Agilent4156C'
 # Device information
+sample = 'E0325t-1-1'
 mesa = 'D56.3'
 distance_between_mesa = 1300
-last_X = 11
+last_X = 4
 last_Y = 4
-subs_width = 13500  # approximately
-subs_height = 4500  # approx
+subs_width = 6500  # approximately
+subs_height = 6500  # approx
 # Use theta.py
-# (theta_diagonal, x00_subs, y00_subs) = (21.44501633542317, -890.5720364478045, -878.5148369616054)  # D169
-(theta_diagonal, x00_subs, y00_subs) = (21.44501633542317, -890.5720364478045 + 300, -878.5148369616054)  # D56.3
-# (theta_diagonal, x00_subs, y00_subs) = (21.44501633542317, -890.5720364478045 + 600, -878.5148369616054)  # D16.7
-# (theta_diagonal, x00_subs, y00_subs) = (21.44501633542317, -890.5720364478045 + 900, -878.5148369616054)  # D5.??
+(theta_diagonal, x00_subs, y00_subs) = (43.9395886298078, -353.9133081057827, -23.85219452421734)  # D169
+if mesa == 'D56.3':
+    x00_subs += 300
+if mesa == 'D16.7':
+    x00_subs += 600
+if mesa == 'D5.54':
+    x00_subs += 900
 # Measurement configurations
 z_contact = 12000
 z_separate = z_contact - 100
 # meas_XYs = [(1, 1), (3, 2), (1, 2), (2, 1)]
-meas_XYs = [(X, 1) for X in range(1, 11)] + [(X, 2) for X in reversed(range(3, 12))] + \
-           [(X, 3) for X in range(3, 12)] + [(X, 4) for X in reversed(range(2, 11))]
+meas_XYs = [(X, 1) for X in range(3, last_X+1)] + [(X, 2) for X in reversed(range(1, last_X+1))] + \
+           [(X, 3) for X in range(1, last_X+1)] + [(X, 4) for X in reversed(range(1, last_X+1))]
 # meas_Vs = [0.1, -0.1, 0.2, -0.2, 0.3, -0.3, -0.4, 0.4, -0.5, 0.5]
-meas_Vs = [0.1, -0.1, 0.2, -0.2, 0.3, -0.3]
+meas_Vs = [0.1, -0.1]
 
 # Initialize -------------------------------------------------------------------
 rm = visa.ResourceManager()
@@ -48,8 +52,9 @@ def rotate_vector(x, y, theta_deg):
     return math.cos(theta_rad)*x - math.sin(theta_rad)*y, math.sin(theta_rad)*x + math.cos(theta_rad)*y
 
 # Measure ----------------------------------------------------------------------
+first_measurement = True
 try:
-    s.velocity = 15
+    s.velocity = 25
     s.moveZ(z_separate - 100)
     s.velocity = 1
     input('Set substrate left bottom edge as home.')
@@ -65,10 +70,13 @@ try:
         (x_next_from_home, y_next_from_home) = rotate_vector(-x_next_subs, -y_next_subs, theta_pattern_tilled)
         s.move_to_xy_from_home(x_next_from_home, y_next_from_home)
         s.moveZ(z_contact)  # s.contact()
+        if first_measurement:
+            input('Contact the prober.')
+            first_measurement = False
         for V in meas_Vs:
             t0 = time.strftime('%Y%m%d-%H%M%S')
             Vs, Is, aborted = a.double_sweep_from_zero(2, 1, V, V/1000, 10e-6, 10e-3)
-            filename = 'double-sweep_{}_E0326-2-1_X{}_Y{}_{}_{}V.csv'.format(t0, X, Y, mesa, V)
+            filename = 'double-sweep_{}_{}_X{}_Y{}_{}_{}V.csv'.format(t0, sample, X, Y, mesa, V)
             points = len(Vs)
             with open(datadir + '\\' + filename, 'w') as f:
                 Vs = [str(elem) for elem in Vs]
@@ -80,15 +88,15 @@ try:
                 f.write(Is)  # I, I, I, ...
                 f.write('\n')
             with open(datadir + '\\double-sweep_params.csv', 'a') as f:
-                f.write('t0={},sample=E0326-2-1,X={},Y={},xpos={},ypos={},mesa={},status=255,measPoints={},comp=0.01,instr=SUSS PA200, originalFileName={}\n'.
+                f.write('t0={},sample=E0326-2-1,X={},Y={},xpos={},ypos={},mesa={},status=255,measPoints={},comp=0.01,instr=SUSS PA300, originalFileName={}\n'.
                        format(t0, X, Y, x_next_subs, y_next_subs, mesa, points, filename))
-                # t0=20150717-125846, sample=E0326-2-1,X=5,Y=3,xpos=5921.5,ypos=3031.5,mesa=D56.3,status=255,measPoints=101,comp=0.01,instr=SUSS PA200, originalFileName=double-sweep_20150717-125846_E0326-2-1_X5_Y3_D56.3_0.1V.csv
+                # t0=20150717-125846, sample=E0326-2-1,X=5,Y=3,xpos=5921.5,ypos=3031.5,mesa=D56.3,status=255,measPoints=101,comp=0.01,instr=SUSS PA300, originalFileName=double-sweep_20150717-125846_E0326-2-1_X5_Y3_D56.3_0.1V.csv
             if aborted:
                 break
 except:
     with open(os.path.expanduser('~') + r'\Dropbox\work\0instr_report.txt', 'w') as f:
         f.write(traceback.format_exc() + '\n')
-        # del Vs, Is  # Because they are too long
+        del Vs, Is  # Because they are too long
         f.write('\n---------- globals() ----------\n{}\n'.format(globals()))
         f.write('\n---------- locals() ----------\n{}\n'.format(locals()))
     raise
