0ddafcac7988333da7456d9426e67f7f80010d83
wataash <expllion@gmail.com>
2015-08-03 15:21:08 +0900

    Develop plot_I-V.py

---
+++ plot_examples.py
@@ -0,0 +1,7 @@
+# http://matplotlib.org/examples/pylab_examples/subplots_demo.html
+# plt.scatter( [ [1,2], [3,4] ] , [ [5,6], [7,8]  ]  )
+# plt.semilogy( [ [1,2,3], [4,5,6] ] , [ [7,8,9], [10,11,12] ] )
+# plt.semilogy( [ [1,2],[3,4],[5,6] ] )
+# plt.semilogy( [1,2],[3,4],[5,6] )  # error
+# plt.semilogy( [ [1,2],[3,4] ] )
+# plt.semilogy( [1,2],[3,4] )
