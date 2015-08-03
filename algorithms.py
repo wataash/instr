9e5a5342424ce8410411dde9278baad751350b97
wataash <expllion@gmail.com>
2015-08-03 18:46:54 +0900

    Develop

---
+++ algorithms.py
@@ -0,0 +1,52 @@
+from operator import itemgetter
+
+
+def ave_xyz(lists):
+    """
+    # in
+    # [[x0, x1, ...],
+    #  [y0, y1, ...],
+    #  [z0, z1, ...],
+    # ...
+    # ]
+    # [[0, 1, 2, 1, 2],
+    #  [1, 2, 3, 4, 5],
+    #  [0, 1, 2, 3, 4],
+    #  [10, 11, 12, 13, 14]
+    # ]
+    # out
+    # [(0, 1, 2),
+    #  (1, 3, 4),
+    #  (0, 2, 3),
+    #  (10, 12, 13)
+    # ]
+    :param lists:
+    :return:
+    """
+    xyzs = [xyz for xyz in zip(*lists)]
+    xyzs_sorted = sorted(xyzs, key=itemgetter(0))
+    res_lists_transposed = []  # [[x0, y0, z0], ... ]
+    num_variables = len(xyzs[0])
+    prev_x = -999999999
+    num_x = 0
+    sum_yz = [0] * (num_variables - 1)
+    for xyz in xyzs_sorted:
+        if xyz[0] != prev_x and num_x != 0:
+            res_lists_transposed.append([prev_x] + [y/num_x for y in sum_yz])  # y: y or z or ...
+            num_x = 0
+            sum_yz = [0] * (num_variables - 1)
+        for i, y in enumerate(xyz[1:]):  # y: y or z or ...
+            sum_yz[i] += y
+        prev_x = xyz[0]
+        num_x += 1
+    res_lists_transposed.append([prev_x] + [y/num_x for y in sum_yz])  # last x
+    return list(zip(*res_lists_transposed))
+
+if __name__ == '__main__':
+    ave = ave_xyz([
+        [0, 1, 2, 1, 2],
+        [1, 2, 3, 4, 5],
+        [0, 1, 2, 3, 4],
+        [10, 11, 12, 13, 14]
+    ])
+    print(ave)
--- plot_I-V.py
+++ plot_I-V.py
@@ -1,14 +1,20 @@
 from collections import defaultdict
 import math
 import os
-# import sqlite3
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+from algorithms import ave_xyz
 
 # Prefix 'd': dictionary
 
 sample = 'E0326-2-1'
 dia = 169
+Xmin = 1
+Xmax = 11
+Ymin = 1
+Ymax = 4
 datadir = os.environ['appdata'] + r'\Instr\Agilent4156C'
 
 area = math.pi * (dia/2)**2  # um^2
@@ -25,19 +31,20 @@ for fname in [fname_all for fname_all in os.listdir(datadir) if
         Y = int(tmp[4][1:])
         D = float(tmp[5][1:])
         read = f.read().split()
-        newV = [float(t) for t in read[0].split(',')]
-        newJ = [float(J)/area for J in read[1].split(',')]
-        newR = [(V/J if abs(V) > 0.010 else None) for (V, J) in zip(newV, newJ)]
-        d_V[(X, Y, D)] += newV
-        d_J[(X, Y, D)] += newJ
-        d_RA[(X, Y, D)] += newR
+        newVs = [float(t) for t in read[0].split(',')]
+        newJs = [float(J)/area for J in read[1].split(',')]
+        # newRs = [(V/J if abs(V) > 0.010 else None) for (V, J) in zip(newVs, newJs)]
+        newRs = [(V/J if abs(V) > 0.010 else 0) for (V, J) in zip(newVs, newJs)]
+        (newVs, newJs, newRs) = ave_xyz((newVs, newJs, newRs))  # Take average
+        d_V[(X, Y, D)] += newVs
+        d_J[(X, Y, D)] += newJs
+        d_RA[(X, Y, D)] += newRs
         print(X, Y, D)
 
+# Average
+for (X, Y) in [(X, Y) for Y in range(Ymin, Ymax+1) for X in range(Xmin, Xmax+1)]:
+    (d_V[(X, Y, dia)], d_J[(X, Y, dia)], d_RA[(X, Y, dia)]) = ave_xyz((d_V[(X, Y, dia)], d_J[(X, Y, dia)], d_RA[(X, Y, dia)]))
 
-Xmin = 1
-Xmax = 11
-Ymin = 1
-Ymax = 4
 
 numX = Xmax - Xmin + 1
 numY = Ymax - Ymin + 1
@@ -52,13 +59,15 @@ for (rowi, coli) in [(rowi, coli) for rowi in range(numY) for coli in range(numX
     # (09)XminYmin 19(Xmin+1)Ymin ... (99)XmaxYmin
     X = coli + Xmin
     Y = Ymax - rowi
-    p = axarr[rowi, coli].plot(d_V[(X, Y, dia)], d_J[(X, Y, dia)], 'b')
+    # p = axarr[rowi, coli].plot(d_V[(X, Y, dia)], d_J[(X, Y, dia)], 'b')
+    p = axarr[rowi, coli].plot(d_V[(X, Y, dia)], np.gradient(d_J[(X, Y, dia)], d_V[(X, Y, dia)]), 'b')
     # p = axarr[rowi, coli].plot(d_V[(X, Y, dia)], d_RA[(X, Y, dia)], 'r')
     axarr[rowi, coli].set_xticks([])
     axarr[rowi, coli].set_yticks([])
     axarr[rowi, coli].set_xlim([-0.5, 0.5])
-    axarr[rowi, coli].set_ylim([-1e-11, 1e-11])  # J [A/um^2]
+    # axarr[rowi, coli].set_ylim([-1e-11, 1e-11])  # J [A/um^2]
     # axarr[rowi, coli].set_ylim([0, 2e12])  # RA [ohm um^2]
+    axarr[rowi, coli].set_ylim([-1e-13, 1e-13])  # dJ/dV [A/um^2/V]
 # plt.show()
 plt.savefig(os.path.expanduser('~') + r'\Desktop\tmp.png', dpi=300, transparent=True)
 
