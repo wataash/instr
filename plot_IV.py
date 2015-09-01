6f8d0bb43494c559fabe54c3471cce1a5fa9cdd5
wataash <expllion@gmail.com>
2015-09-01 17:29:55 +0900

    Bug fix plot_IV.py

--- plot_IV.py
+++ plot_IV.py
@@ -8,9 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Prefix 'd': dictionary
-
-
 # Configurations ---------------------------------------------------------------
 #conf = defaultdict(str)
 conf = {}
@@ -37,10 +34,10 @@ cursor = sqlite3_connection.cursor()
 
 # Plot -------------------------------------------------------------------------
 f, axarr = plt.subplots(numY, numX, figsize=(numX, numY), facecolor='w')  # Takes long time
-f.subplots_adjust(top=1, bottom=0, left=0, right=1, wspace=0, hspace=0)
+#f.subplots_adjust(top=1, bottom=0, left=0, right=1, wspace=0, hspace=0)
 
-for Y in range(conf['Xmin'], conf['Xmax'] + 1):
-    for X in range(conf['Ymin'], conf['Ymax'] + 1):
+for Y in range(conf['Ymin'], conf['Ymax'] + 1):
+    for X in range(conf['Xmin'], conf['Xmax'] + 1):
         print('execute')
         t0s = cursor.execute('''
             SELECT t0 FROM parameters
@@ -55,8 +52,14 @@ for Y in range(conf['Xmin'], conf['Xmax'] + 1):
         #    WHERE sample=? AND mesa=? AND X=? AND Y=?
         #    ''', (conf['sample'], conf['mesa'], X, Y)).fetchall()
 
-        if t0s == []:
-            continue
+        # rowi coli X Y matrix (if num_X = num_Y = 9)
+        # 00XminYmax     ...                90XmaxYmax
+        # ...
+        # 80Xmin(Ymin+1) ...
+        # 90XminYmin     91(Xmin+1)Ymin ... 99XmaxYmin
+        coli = -conf['Xmin'] + X  # privious: -X TODO check
+        rowi = conf['Ymax'] - Y
+        axarr[rowi, coli].locator_params(nbins=5)  # number of ticks
 
         for t0 in t0s:
             tmp = cursor.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall()
@@ -67,23 +70,20 @@ for Y in range(conf['Xmin'], conf['Xmax'] + 1):
             J = VIs.transpose()[1]/area
             RA = V/J
 
-            # rowi coli X Y matrix (if num_X = num_Y = 9)
-            # 00XminYmax     ...                90XmaxYmax
-            # ...
-            # 80Xmin(Ymin+1) ...
-            # 90XminYmin     91(Xmin+1)Ymin ... 99XmaxYmin
-            coli = -conf['Xmin'] + X  # privious: -X TODO check
-            rowi = conf['Ymax'] - Y
-
             print('plot')
-            axarr[rowi, coli].plot(V, J, 'b', linewidth=0.2)
-            #axarr[rowi, coli].plot(V, RA, 'r', linewidth=0.2)
+            #axarr[rowi, coli].plot(V, J, 'b', linewidth=0.5)
+            #axarr[rowi, coli].plot(V, RA, 'r', linewidth=0.5)
+            axarr[rowi, coli].plot(V, J, 'r')
             #axarr[rowi, coli].plot(V, np.gradient(J, V), 'b', linewidth=0.1)
             axarr[rowi, coli].set_xticks([])
-            axarr[rowi, coli].set_yticks([])
+            #axarr[rowi, coli].set_yticks([])
             axarr[rowi, coli].set_xlim([conf['plot_V_min'], conf['plot_V_max']])
-            # axarr[rowi, coli].set_ylim([-1e-11, 1e-11])  # J [A/um^2]
-            # axarr[rowi, coli].set_ylim([0, 2e12])  # RA [ohm um^2]
+            #axarr[rowi, coli].set_ylim([-1e-6, 1e-6])  # J [A/um^2]
+            #axarr[rowi, coli].set_ylim([0, 1e12])  # RA [ohm um^2]
             #axarr[rowi, coli].set_ylim([-1e-13, 1e-13])  # dJ/dV [A/um^2/V]
 
-plt.savefig(os.path.expanduser('~') + r'\Desktop\tmp.png', dpi=300, transparent=True)
+        if t0s == []:
+            axarr[rowi, coli].set_xticks([])
+            axarr[rowi, coli].set_yticks([])
+
+plt.savefig(os.path.expanduser('~') + r'\Desktop\J1.png', dpi=300, transparent=True)
