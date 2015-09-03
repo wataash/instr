d721dcd84f4416dda72915ffa55e347dcfa4f195
wataash <expllion@gmail.com>
2015-09-04 00:04:15 +0900

    Develop

--- calculate_R_near_0V.py
+++ calculate_R_near_0V.py
@@ -1,14 +1,13 @@
 ﻿import json
-#import math
-#import os
+import os
 import sqlite3
 
 #import matplotlib.pyplot as plt
 import numpy as np
-#import statsmodels.api as sm
+import statsmodels.api as sm
 
 sample = 'E0339'
-sqlite3_file_path = r'C:\Users\wsh14\Documents\instr_data/IV.sqlite3'
+sqlite3_file_path = os.path.expanduser('~') + r'\Documents\instr_data/IV.sqlite3'
 
 sqlite3_connection = sqlite3.connect(sqlite3_file_path)
 cursor_t0 = sqlite3_connection.cursor()
@@ -16,15 +15,19 @@ cursor_IV = sqlite3_connection.cursor()
 
 for Y in range(1, 17 + 1):
     for X in range(1, 17 + 1):
-        VIs = np.array([[]])
+        VIs = []
         for t0 in cursor_t0.execute('''
                 SELECT t0 FROM parameters
                 WHERE sample=? AND X=? AND Y=? AND
                 t0 BETWEEN 20150826130701 AND 20150826131040
                 ''', (sample, X, Y)):
-            tmp = cursor_IV.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall()
-            #VIs += tmp
-            VIs = np.append(VIs, tmp)
+            VIs += cursor_IV.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall()
+        if VIs == []:
+            continue
+        [V, I] = list(zip(*VIs))
+        model = sm.OLS(I, V)
+        results = model.fit()
+        tmp = results.params
 
 #for t0 in cursor_t0.execute('SELECT t0 FROM parameters'):
 #    print(t0)
