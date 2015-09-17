58189d7491887062d2b553fc0b1a3738c06dd8a8
wataash <expllion@gmail.com>
2015-09-17 09:34:42 +0900

    Remove debug_mode from pa300_plot_matrix.py. (because achieved fast SQLite query after make index on t0.)

--- pa300_plot_matrix.py
+++ pa300_plot_matrix.py
@@ -11,11 +11,6 @@ import numpy as np
 from lib.algorithms import remove_X_near_0
 
 
-# Fast debag mode (SQL query is slow if database is large)
-debug_mode = False
-if debug_mode:
-    print('*'*20, 'Debug mode', '*'*20)
-
 # Configurations ---------------------------------------------------------------
 sqlite3_file = os.path.expanduser('~') + '/Documents/instr_data/IV.sqlite3'
 
@@ -50,21 +45,21 @@ unit = dict_unit[var_y]
 ylim_nega = dict_ylim_nega[var_y]
 ylim_pos = dict_ylim_pos[var_y]
 
-png_file_name = os.path.expanduser('~')
+# Set file name without extension (base)
+save_dir = os.path.expanduser('~/Desktop')
 if fix_y_range:
     # E0339_D169_RA_1E11_ohmm2_-0.2V_0.2V.png
-    png_file_name += '/Desktop/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_{ylim_nega}_{ylim_pos}_{unit}_{min_V}V_{max_V}V.png'. \
+    save_file_name_base = save_dir + '/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_{ylim_nega}_{ylim_pos}_{unit}_{min_V}V_{max_V}V'. \
         format(sample=sample, mesa=mesa, min_X=min_X, max_X=max_X, min_Y=min_Y, max_Y=max_Y, var_y=var_y, unit=unit, ylim_nega=ylim_nega, ylim_pos=ylim_pos, min_V=min_V, max_V=max_V)
 else:
     # E0339_D169_RA_auto_ohmm2_-0.2V_0.2V.png
-    png_file_name += '/Desktop/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_auto_{unit}_{min_V}V_{max_V}V.png'. \
+    save_file_name_base = save_dir + '/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_auto_{unit}_{min_V}V_{max_V}V'. \
         format(sample=sample, mesa=mesa, min_X=min_X, max_X=max_X, min_Y=min_Y, max_Y=max_Y, var_y=var_y, unit=unit, min_V=min_V, max_V=max_V)
-print('Save to:', png_file_name)
+print('Save to: {}.ext'.format(save_file_name_base))
 
 # Connect to database
-if not debug_mode:
-    sqlite3_connection = sqlite3.connect(sqlite3_file)
-    cursor = sqlite3_connection.cursor()
+sqlite3_connection = sqlite3.connect(sqlite3_file)
+cursor = sqlite3_connection.cursor()
 
 
 # Plot -------------------------------------------------------------------------
@@ -75,22 +70,10 @@ if fix_y_range:
 
 for Y in range(min_Y, max_Y + 1):
     for X in range(min_X, max_X + 1):
-        print('Processing X{}Y{}.'.format(X, Y))
-        if debug_mode:
-            t0s = [1234]
-        else:
-            print('Executing SQLite command.')
-            t0s = cursor.execute('''
-                SELECT t0 FROM parameters
-                WHERE sample=? AND mesa=? AND X=? AND Y=?
-                ''', (sample, mesa, X, Y)).fetchall()
-
-        # Slow because of searching in all data in IV
-        #    cursor.execute('''
-        #    SELECT V, I FROM IV
-        #    INNER JOIN parameters ON IV.t0=parameters.t0
-        #    WHERE sample=? AND mesa=? AND X=? AND Y=?
-        #    ''', (sample, mesa, X, Y)).fetchall()
+        print('\nProcessing X{}Y{}.'.format(X, Y))
+        print('Executing SQLite command.')
+        t0s = cursor.execute('SELECT t0 FROM parameters WHERE sample=? AND mesa=? AND X=? AND Y=?',
+                             (sample, mesa, X, Y)).fetchall()
 
         # rowi coli X Y matrix (if num_X = num_Y = 9)
         # 00XminYmax     ...                90XmaxYmax
@@ -107,7 +90,6 @@ for Y in range(min_Y, max_Y + 1):
             axarr[rowi, coli].set_yticks([])
             continue
 
-        print('Configuring plot for t0s =', t0s)
         axarr[rowi, coli].locator_params(nbins=5)  # number of ticks
         axarr[rowi, coli].get_yaxis().get_major_formatter().set_powerlimits((0, 0))  # Force exponential ticks
 
@@ -115,12 +97,7 @@ for Y in range(min_Y, max_Y + 1):
         xs = np.array([])  # x axis values
         ys = np.array([])  # y axis values
         for t0 in t0s:
-            print('Querying data.')
-            if debug_mode:
-                VIs_new = [[V, (1e-5 + 1e-6*X)*(V + 0.1*V**2) + 1e-7*Y*math.sin(Y*V/max_V)] for V in np.linspace(min_V, max_V, 101)]
-            else:
-                VIs_new = cursor.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall() # Slow (~1sec)
-            print('Got data.')
+            VIs_new = cursor.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall() # Be sure that t0 has index (else slow)
             VIs_new = np.array(VIs_new)
             if var_y in ['RA', 'R']:
                 VIs_new = remove_X_near_0(VIs_new, 10e-3)
@@ -135,21 +112,12 @@ for Y in range(min_Y, max_Y + 1):
             elif var_y == 'R':
                 Rs_new = Vs_new/(Js_new*area)
                 ys = np.append(ys, Rs_new)
+            elif var_y == 'dJdV':
+                dJdV_new = np.gradient(Js_new, Vs_new)  # TODO: implement
+                ys = np.append(ys, dJdV_new)
 
-        ## Plot data XY
-        #print('Plotting t0 =', t0)
-        #if var_y == 'J':
-        #    axarr[rowi, coli].plot(Vs, Js, 'b', linewidth=0.5)
-        #elif var_y == 'RA':
-        #    axarr[rowi, coli].plot(Vs, RAs, 'r', linewidth=0.5)
-        #elif var_y == 'R':
-        #    axarr[rowi, coli].plot(Vs, Rs, 'r', linewidth=0.5)
-        #elif var_y == 'dJdV':
-        #    axarr[rowi, coli].plot(Vs, np.gradient(Js, Vs), 'b', linewidth=0.1)
-
-        # Scatter XY
-        print('Plotting t0 =', t0)
-
+        # Plot
+        #axarr[rowi, coli].plot(xs, ys, 'b', linewidth=0.5)
         axarr[rowi, coli].scatter(xs, ys, s=0.1, c=list(range(len(xs))), cmap=cm.rainbow, edgecolor='none')  # s: size
 
         axarr[rowi, coli].set_xticks([])
@@ -158,8 +126,8 @@ for Y in range(min_Y, max_Y + 1):
             axarr[rowi, coli].set_yticks([])
             axarr[rowi, coli].set_ylim([-float(ylim_nega), float(ylim_pos)])
 
-        print()  # newline
 
-
-plt.savefig(png_file_name, dpi=300, transparent=True)
-plt.savefig(png_file_name.replace('.png', '.pdf'), transparent=True)  # dpi is ignored, transparent as well?
+print('Saving', save_file_name_base + '.png')
+plt.savefig(save_file_name_base + '.png', dpi=300, transparent=True)
+print('Saving', save_file_name_base + '.pdf')
+plt.savefig(save_file_name_base + '.pdf', transparent=True)  # dpi is ignored, transparent as well?
