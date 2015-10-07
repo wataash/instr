c14307f1a80701fc2089ec17c284279076767c6e
wataash <expllion@gmail.com>
2015-10-07 14:34:41 +0900

    Update

--- pa300_plot_matrix.py
+++ pa300_plot_matrix.py
@@ -1,5 +1,6 @@
 ﻿# Std libs
 from collections import defaultdict
+import json
 import math
 import os
 import sqlite3
@@ -12,80 +13,80 @@ from lib.algorithms import remove_X_near_0
 from lib.algorithms import remove_xyz_by_x
 
 
-# Configurations ---------------------------------------------------------------
+# Get configrations --------------------------------------
 # None: auto (get from databse)
+with open(os.path.expanduser('~') + '/Dropbox/master-db/src-master-db/pa300_plot_matrix.json') as f:
+    j = json.load(f)
+db_params = os.path.expanduser('~') + '/' + j['db_params']
+db_IVs = os.path.expanduser('~') + '/' + j['db_IVs']
+conn_params = sqlite3.connect(db_params)
+conn_IVs = sqlite3.connect(db_IVs)
+cur_params = conn_params.cursor()
+cur_IVs = conn_IVs.cursor()
 
-sqlite3_file = os.path.expanduser('~') + '/Documents/instr_data/IV.sqlite3'
+sample, X_min, X_max, Y_min, Y_max = \
+    cur_params.execute('SELECT sample, min_X, max_X, min_Y, max_Y \
+                        FROM samples WHERE id=?',
+                        (str(j['sample_id']))).fetchone()
+mesa, area = \
+    cur_params.execute('SELECT name, area \
+                        FROM mesas WHERE id=?',
+                        (str(j['mesa_id']),)).fetchone()
 
-# Device
-sample = 'E0326-2-1'
-mesa = ['D169', 'D56.3', 'D16.7', 'D5.54'][0]
-dia = {'D169': 169e-6, 'D56.3': 56.3e-6, 'D16.7': 16.7e-6, 'D5.54': 5.54e-6}[mesa] # diameter [m]
-area = math.pi * (dia/2)**2  # [m^2]
+remove_V = 0.020 # for R, RA  # TODO auto calculate trim V
+var_y = j['var_ys'][j['var_y_index']]
+unit = j['var_y_units'][j['var_y_index']]
 
-# Bug: Error when (min_X == max_X) or (min_Y == max_Y) at axarr[rowi, coli]
-# They must be (min_X < max_X) and (min_Y < max_Y).
-min_X, max_X, min_Y, max_Y = (None, None, None, None)
+## TODO debug
+#X_min, X_max, Y_min, Y_max = 1, 2, 1, 2
+#area = 1
 
-# Plot config
-V_min, V_max = (None, None)
-remove_V = 0.020 # for R, RA
-remove_V = None
-var_y = ['J', 'RA', 'R'][0]
-dict_unit = {'J': 'Am2', 'RA': 'ohmm2', 'R': 'ohm'}
-fix_ylim = False
-dict_ylim_nega = {'J': '-1E-5', 'RA': '0', 'R': '0'}
-dict_ylim_pos = {'J': '1E-5', 'RA': '1E-1', 'R': '1E6'}
-
-
-# Calculations -----------------------------------------------------------------
 # Number of columns and rows in matrix plot
-numX = max_X - min_X + 1
-numY = max_Y - min_Y + 1
+numX = X_max - X_min + 1
+numY = Y_max - Y_min + 1
 
-unit = dict_unit[var_y]
-ylim_nega = dict_ylim_nega[var_y]
-ylim_pos = dict_ylim_pos[var_y]
+if j['fix_y']:
+    # TODO: test
+    ylim_nega = j['ylims_nega'][j['var_y_index']]
+    ylim_pos = j['ylims_pos'][j['var_y_index']]
 
 # Set png file name
 save_dir = os.path.expanduser('~/Desktop')
-if fix_ylim:
+if j['fix_y']:
     # E0339_D169_RA_1E11_ohmm2_-0.2V_0.2V.png
-    save_file_name_base = save_dir + '/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_{ylim_nega}_{ylim_pos}_{unit}_{min_V}V_{max_V}V.png'. \
-        format(sample=sample, mesa=mesa, min_X=min_X, max_X=max_X, min_Y=min_Y, max_Y=max_Y, var_y=var_y, unit=unit, ylim_nega=ylim_nega, ylim_pos=ylim_pos, min_V=V_min, max_V=V_max)
+    save_file_name_base = save_dir + '/{sample}_{mesa}_X{X_min}-{X_max}_Y{Y_min}-{Y_max}_{var_y}_{ylim_nega}_{ylim_pos}_{unit}_{min_V}V_{max_V}V.png'. \
+        format(sample=sample, mesa=mesa, X_min=X_min, X_max=X_max, Y_min=Y_min, Y_max=Y_max, var_y=var_y, unit=unit, ylim_nega=ylim_nega, ylim_pos=ylim_pos, min_V=j['V_min'], max_V=j['V_max'])
 else:
     # E0339_D169_RA_auto_ohmm2_-0.2V_0.2V.png
-    save_file_name_base = save_dir + '/{sample}_{mesa}_X{min_X}-{max_X}_Y{min_Y}-{max_Y}_{var_y}_auto_{unit}_{min_V}V_{max_V}V.png'. \
-        format(sample=sample, mesa=mesa, min_X=min_X, max_X=max_X, min_Y=min_Y, max_Y=max_Y, var_y=var_y, unit=unit, min_V=V_min, max_V=V_max)
+    save_file_name_base = save_dir + '/{sample}_{mesa}_X{X_min}-{X_max}_Y{Y_min}-{Y_max}_{var_y}_auto_{unit}_{min_V}V_{max_V}V.png'. \
+        format(sample=sample, mesa=mesa, X_min=X_min, X_max=X_max, Y_min=Y_min, Y_max=Y_max, var_y=var_y, unit=unit, min_V=j['V_min'], max_V=j['V_max'])
 print('Save to:', save_file_name_base)
-
-# Connect to database
-sqlite3_connection = sqlite3.connect(sqlite3_file)
-cursor = sqlite3_connection.cursor()
 
 
 # Plot -------------------------------------------------------------------------
 print('Making subplots frame...')
-# Takes long time. figsize: inches. (300dpi -> about (300numX px, 300numY px)
+# Takes long time. figsize: inches. (300dpi -> about (300*numX px, 300*numY px)
+# Bug: Error when (X_min == X_max) or (Y_min == Y_max) at axarr[rowi, coli]
+# They must be (X_min < X_max) and (Y_min < Y_max).
 f, axarr = plt.subplots(numY, numX, figsize=(numX, numY), facecolor='w')
-if fix_ylim:
+if j['fix_y']:
     f.subplots_adjust(top=1, bottom=0, left=0, right=1, wspace=0, hspace=0)
 else:
     f.subplots_adjust(top=0.99, bottom=0.01, left=0.01, right=0.99, wspace=0, hspace=0)
 
-for Y in range(min_Y, max_Y + 1):
-    for X in range(min_X, max_X + 1):
+for Y in range(Y_min, Y_max + 1):
+    for X in range(X_min, X_max + 1):
         print('Processing X{}Y{}.'.format(X, Y))
-        t0s = cursor.execute('SELECT t0 FROM parameters WHERE sample=? AND mesa=? AND X=? AND Y=?',
-                             (sample, mesa, X, Y)).fetchall()
+        t0s = cur_params.execute('SELECT t0 FROM params WHERE sample=? AND mesa=? AND X=? AND Y=?',
+                                  (sample, mesa, X, Y)).fetchall()
 
         # rowi coli X Y matrix (if num_X = num_Y = 9)
         # 00XminYmax     ...                90XmaxYmax
         # ...
         # 80Xmin(Ymin+1) ...
         # 90XminYmin     91(Xmin+1)Ymin ... 99XmaxYmin
-        coli = -min_X + X
-        rowi = max_Y - Y
+        coli = -X_min + X
+        rowi = Y_max - Y
         ax = axarr[rowi, coli]
 
         if t0s == []:
@@ -108,7 +109,7 @@ for Y in range(min_Y, max_Y + 1):
         xs = np.array([])  # x axis values
         ys = np.array([])  # y axis values
         for t0 in t0s:
-            VIs_new = cursor.execute('SELECT V, I FROM IV WHERE t0=?', t0).fetchall() # Be sure that t0 has index (else slow)
+            VIs_new = cur_IVs.execute('SELECT V, I FROM IVs WHERE t0=?', t0).fetchall() # Be sure that t0 has index (else slow)
             VIs_new = np.array(VIs_new)
             if var_y in ['RA', 'R']:
                 VIs_new = remove_X_near_0(VIs_new, remove_V)  # TODO: auto determine divergence near 0
@@ -126,16 +127,16 @@ for Y in range(min_Y, max_Y + 1):
             elif var_y == 'dJdV':
                 dJdV_new = np.gradient(Js_new, Vs_new)  # TODO: implement
                 ys = np.append(ys, dJdV_new)
-        xys_in_range = remove_xyz_by_x(lambda x: not V_min < x < V_max, xs, ys)
+        xys_in_range = remove_xyz_by_x(lambda x: not j['V_min'] < x < j['V_max'], xs, ys)
         ys_in_range = xys_in_range[1]
 
         # Plot
         #ax.plot(xs, ys, 'b', linewidth=0.5)
-        ax.scatter(xs, ys, s=0.1, c=list(range(len(xs))), cmap=cm.rainbow, edgecolor='none')  # s: size
+        ax.scatter(xs, ys, s=1, c=list(range(len(xs))), cmap=cm.rainbow, edgecolor='none')  # s: size
 
         ax.set_xticks([])
-        ax.set_xlim([V_min, V_max])
-        if fix_ylim:
+        ax.set_xlim([j['V_min'], j['V_max']])
+        if j['fix_y']:
             ax.set_yticks([])
             ax.set_ylim([-float(ylim_nega), float(ylim_pos)])
         elif var_y in ['J', 'dJdV']:
@@ -159,6 +160,6 @@ for Y in range(min_Y, max_Y + 1):
             
 
 print('Saving', save_file_name_base + '.png')
-plt.savefig(save_file_name_base + '.png', dpi=600, transparent=True)
+plt.savefig(save_file_name_base + '.png', dpi=200, transparent=True)
 #print('Saving', save_file_name_base + '.pdf')
 #plt.savefig(save_file_name_base + '.pdf', transparent=True)  # dpi is ignored, transparent as well?
