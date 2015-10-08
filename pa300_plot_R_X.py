9b1f0e8c802be5c1b78155f36097a6e15f575849
wataash <expllion@gmail.com>
2015-10-08 17:15:29 +0900

    Update plots

--- pa300_plot_R_X.py
+++ pa300_plot_R_X.py
@@ -1,38 +1,36 @@
-# Std libs
+﻿# Std libs
 import json
 import os.path
 import sqlite3
 # Non-std libs
 import matplotlib.pyplot as plt
+# My libs
+import constants as c
 
-
-with open(os.path.expanduser('~') + '/Dropbox/master-db/src-master-db/pa300_config.json') as f:
-    j = json.load(f)
-sample = j['sample']
-mesas = j['plt_mesas']
-
-conn_params = sqlite3.connect(j['db_params_file'].replace('home', os.path.expanduser('~')))
+conn_params = sqlite3.connect(c.sql_params_dropbox)
 cur_params = conn_params.cursor()
 
-#sample, X_min, X_max, Y_min, Y_max = \
-#    cur_params.execute('SELECT sample, min_X, max_X, min_Y, max_Y \
-#                        FROM samples WHERE id=?',
-#                        (sample,)).fetchone()
-#mesa, area = \
-#    cur_params.execute('SELECT name, area \
-#                        FROM mesas WHERE id=?',
-#                        (str(j['mesa_id']),)).fetchone()
-
 Xs = {}
+Ys = {}
 Rs = {}
-for mesa in mesas:
-    XYRs = cur_params.execute('SELECT X,Y,R FROM resistance WHERE sample=? AND mesa=? AND Y>=9', (sample, mesa)).fetchall()
-    Xs[mesa] = [XYR[0] for XYR in XYRs]
-    Rs[mesa] = [XYR[2] for XYR in XYRs]
-    plt.semilogy(Xs[mesa],Rs[mesa], 'o', label=mesa)
-    plt.legend(loc=2)
+c.p_mesas = c.p_mesas[3:4]
+
+fig = plt.figure()
+ax = plt.gca()
+ax.set_yscale('log')
+
+for mesa in c.p_mesas:
+    XYRs = cur_params.execute(
+        'SELECT X,Y,R FROM resistance WHERE sample=? AND mesa=? AND Y>=1',
+        (c.p_sample, mesa)).fetchall()
+    if XYRs == []:
+        continue
+    Xs[mesa], Ys[mesa], Rs[mesa] = zip(*XYRs)
+    ax.scatter(Xs[mesa], Rs[mesa], c=Ys[mesa], marker='x', label=mesa)
+    for i in range(len(Xs[mesa])):
+        ax.annotate(Ys[mesa][i], (Xs[mesa][i], Rs[mesa][i]))
+    ax.legend(loc=2)
     #plt.ylim([1e3, 1e8])
 plt.show()
 
 conn_params.close()
-conn_IVs.close()
--- pa300_plot_R_XY.py
+++ pa300_plot_R_XY.py
@@ -1,71 +1,86 @@
-﻿import math
+﻿# Std libs
+import math
 import os
 import sqlite3
-
+# Non-std libs
 from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
-#import numpy as np
+# MyLibs
+import constants as c
 
 
-# Configurations ---------------------------------------------------------------
-#sqlite3_file = os.path.expanduser('~') + '/Documents/instr_data/IV.sqlite3'
-
-# Device data
-#sample = 'E0339 X9-12 Y13-16'
-#mesa = ['D169', 'D56.3', 'D16.7', 'D5.54'][0]
-#var_y = ['R', 'RA'][0]
-#c_min = {'R': 100, 'RA': 1e-10}[var_y]
-#c_max = {'R': 1e9, 'RA': 1e-5}[var_y]
-#min_X, max_X, min_Y, max_Y = (1, 17, 1, 17)
+#mesa = c.mesas[4]
+var_z = ['R', 'RA'][1]
+auto_color = True
+c_min = {'R': 1e3, 'RA': 1e-10}[var_z]
+c_max = {'R': 1e5, 'RA': 1e-5}[var_z]
 
 
-# Calculations -----------------------------------------------------------------
-png_file_name = os.path.expanduser('~')
-# E0339_D169_R-XY.png
-png_file_name += '/Desktop/{sample}_{mesa}_R-XY.png'.format(sample=sample, mesa=mesa)
-print('Save to:', png_file_name)
 
-# Connect to database
-#sqlite3_connection = sqlite3.connect(sqlite3_file)
-#cursor = sqlite3_connection.cursor()
+conn_params = sqlite3.connect(c.sql_params_dropbox)
+cur_params = conn_params.cursor()
+
+X_min, X_max, Y_min, Y_max = \
+    cur_params.execute('SELECT X_min, X_max, Y_min, Y_max\
+                        FROM samples WHERE sample=?',
+                        (c.p_sample,)).fetchone()
 
 
 # Plot -------------------------------------------------------------------------
-XYRs = cursor.execute('SELECT X,Y,R FROM resistance WHERE sample=? AND mesa=?', (sample, mesa)).fetchall()
+for mesa in c.p_mesas:
+    XYzs = cur_params.execute(
+               'SELECT X,Y,z FROM resistance WHERE sample=? AND mesa=?'.
+               replace(z, var_z),
+               (c.p_sample, mesa)
+           ).fetchall()
 
-# /300: inches to px (for 300dpi)
-# 1000px w/ colorbar, 3200px w/o
-fig = plt.figure(figsize=(3200/300,3200/300))  
-ax = fig.add_subplot(1,1,1)
+    # /300: inches to px (for 300dpi)
+    # 1000px w/ colorbar, 3200px w/o
+    fig = plt.figure(figsize=(3200/300,3200/300))  
+    ax = fig.add_subplot(1,1,1)
 
-# Ticks and grid
-ax.grid(which='minor', linestyle='-', color='gray')
-ax.set_xticks(list(range(min_X, max_X+1)))
-ax.set_xticks([x + 0.5 for x in range(min_X, max_X)], minor=True)  # (min1,max9) -> 1.5, 2.5, ..., 8.5
-ax.set_yticks(list(range(min_Y, max_Y+1)))
-ax.set_yticks([x + 0.5 for x in range(min_Y, max_Y)], minor=True)  # (min1,max9) -> 1.5, 2.5, ..., 8.5
+    # Ticks and grid
+    ax.grid(which='minor', linestyle='-', color='gray')
+    ax.set_xticks(list(range(X_min, X_max+1)))
+    # (min1,max9) -> 1.5, 2.5, ..., 8.5
+    ax.set_xticks([x + 0.5 for x in range(X_min, X_max)], minor=True)
+    ax.set_yticks(list(range(Y_min, Y_max+1)))
+    # (min1,max9) -> 1.5, 2.5, ..., 8.5
+    ax.set_yticks([x + 0.5 for x in range(Y_min, Y_max)], minor=True)
 
-# Axes
-plt.xlim([min_X - 0.6, max_X + 0.6])
-plt.ylim([min_Y - 0.5, max_Y + 0.5])
-ax.tick_params(labeltop=True, labelright=True)
+    # Axes
+    plt.xlim([X_min - 0.6, X_max + 0.6])
+    plt.ylim([Y_min - 0.5, Y_max + 0.5])
+    ax.tick_params(labeltop=True, labelright=True)
 
-# Plot
-Xs = []
-Ys = []
-Rs = []
-for (X, Y, R) in XYRs:
-    if R is None:
-        continue
-    Xs.append(X)
-    Ys.append(Y)
-    Rs.append(R)
-    txt = '{:.2E}'.format(R).replace('E', '\nE')  # 1.2 \n E+03
-    ax.annotate(txt, xy=(X,Y), verticalalignment='center', horizontalalignment='center')
+    # Plot
+    Xs = []
+    Ys = []
+    zs = []
+    for (X, Y, z) in XYzs:
+        if z is None:
+            continue
+        Xs.append(X)
+        Ys.append(Y)
+        zs.append(z)
+        txt = '{:.2E}'.format(z).replace('E', '\nE')  # 1.2 \n E+03
+        ax.annotate(txt, xy=(X,Y),
+                    verticalalignment='center', horizontalalignment='center')
 
-# vmin, vmax: color scale
-sc = ax.scatter(Xs, Ys, c=Rs, cmap='coolwarm', s=1200, marker='s', norm=LogNorm(), vmin=c_min, vmax=c_max)
+    if auto_color:
+        sc = ax.scatter(
+            Xs, Ys, c=zs, cmap='coolwarm', s=1200, marker='s', norm=LogNorm())
+    else:
+        sc = ax.scatter(
+            Xs, Ys, c=zs, cmap='coolwarm', s=1200, marker='s', norm=LogNorm(),
+            vmin=c_min, vmax=c_max)  # vmin, vmax: color scale
+    #plt.colorbar(sc)
 
-#plt.colorbar(sc)
-
-plt.savefig(png_file_name, transparent=True, dpi=300)
+    file_name = os.path.expanduser(
+        '~/Desktop/{sample}_{mesa}_{var_y}-XY_auto.png'.
+        format(sample=c.p_sample, mesa=mesa, var_y=var_z))
+    if not auto_color:
+        file_name = file_name.replace('auto',
+                                      '{c_min:.0E}_{c_max:1.0E}'.
+                                      format(c_min=c_min, c_max=c_max))
+    plt.savefig(file_name, transparent=True, dpi=300)
