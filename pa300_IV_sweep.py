031dcee00cad25db5a0905a080ed15f2ab963d26
wataash <expllion@gmail.com>
2015-10-24 19:06:11 +0900

    Dev

--- pa300_IV_sweep.py
+++ pa300_IV_sweep.py
@@ -1,4 +1,6 @@
-﻿# Std libs
+﻿debug_mode = False  # Set True while development without instruments.
+
+# Std libs
 from collections import defaultdict
 import math
 import os
@@ -8,7 +10,8 @@ import sqlite3
 import time
 import traceback
 # Non-std libs
-#import visa
+if not debug_mode:
+    import visa
 # My libs
 import constants as c
 from lib.algorithms import rotate_vector
@@ -17,43 +20,49 @@ from lib.agilent4156c import Agilent4156C
 from lib.suss_pa300 import SussPA300
 
 
-# Set True while development without instruments.
-debug_mode = True
+sample = 'test sample'
+if debug_mode:
+    sample = 'debug_sample'
+sql_IVs_local = c.local_dir + 'IVs_' + sample + '.sqlite3'
+
+# Connect to database
+if debug_mode:
+    conn_params = sqlite3.connect(c.sql_params_local_debug)
+    conn_IVs = sqlite3.connect(c.sql_IVs_local_debug)
+else:
+    conn_params = sqlite3.connect(c.sql_params_dropbox)
+    conn_IVs = sqlite3.connect(sql_IVs_local)
+cur_params = conn_params.cursor()
+cur_IVs = conn_IVs.cursor()
+
+mask, X_min, X_max, Y_min, Y_max = \
+    cur_params.execute('SELECT mask, X_min, X_max, Y_min, Y_max \
+                        FROM samples WHERE sample=?',
+                       (sample,)).fetchone()
+
+# parameters
+if debug_mode:
+    mesas = ['debug_mesa']
+else:
+    mesas = c.mesas[mask][2:16]
+
+# default XYs
+dicXYs = defaultdict(lambda: zigzag_XY(X_min, Y_min, X_max, Y_max, 'r'))
+# exception XYs
+#dicXYs['mesa#3'] = zigzag_XY(2, 1, 2, 2, 'r')
+
+agi_comp = 0.01
+agi_Vs = [0.2, -0.2]
 
 # If already calibrated theta
-skip_calibrate_theta = True
+skip_calibrate_theta = False
 if skip_calibrate_theta:
     # TODO: query meas IV now?
     theta_pattern_tilled = -0.05113669581916014
     input('Use theta_pattern_tilled: {}'.format(theta_pattern_tilled))
 
-if debug_mode:
-    c.sw_sample = 'debug_sample'
-    c.sw_mesas = ['debug_mesa']
-    c.sql_params_local = c.sql_params_local_debug
-    c.sw_sql_IVs = c.sql_IVs_local_debug
-else:
-    import visa
-
-# Connect to database
-conn_params = sqlite3.connect(c.sql_params_local)
-cur_params = conn_params.cursor()
-conn_IVs = sqlite3.connect(c.sw_sql_IVs)
-cur_IVs = conn_IVs.cursor()
-
-mask, s_width, s_height, X_min, X_max, Y_min, Y_max = \
-    cur_params.execute('SELECT \
-                        mask, width, height, X_min, X_max, Y_min, Y_max \
-                        FROM samples WHERE sample=?',
-                       (c.sw_sample,)).fetchone()
-
 d_X, d_Y = cur_params.execute('SELECT d_X, d_Y FROM masks WHERE mask=?',
                               (mask,)).fetchone()
-
-# default XYs
-dicXYs = defaultdict(lambda: zigzag_XY(X_min, Y_min, X_max, Y_max, 'r'))
-# exception XYs
-dicXYs['D56.3'] = zigzag_XY(2, 1, 2, 2, 'r')
 
 
 # Initialize -------------------------------------------------------------------
@@ -82,11 +91,14 @@ try:
         # Calibrate theta
         delta_X = X_max - X_min
         delta_Y = Y_max - Y_min
-        if delta_X <= 1 or delta_Y <= 1:
+        if delta_X == 0 and delta_Y == 0:
             input('calibrate theta to 0 mannually.')
             theta_pattern_tilled = 0
         else:
-            theta_diagonal_true = math.atan((delta_Y*d_Y) / (delta_X*d_X)) * 180 / math.pi
+            if delta_X == 0:
+                theta_diagonal_true = 90
+            else:
+                theta_diagonal_true = math.atan((delta_Y*d_Y) / (delta_X*d_X)) * 180 / math.pi
             if debug_mode:
                 theta_diagonal_tilled = theta_diagonal_true + random.gauss(0, 3)
             else:
@@ -99,7 +111,7 @@ try:
             print('theta_pattern_tilled:', theta_pattern_tilled)
 
     # Measure I-Vs.  Be sure separate!
-    for mesa in c.sw_mesas:
+    for mesa in mesas:
         print('mesa:', mesa)
         m_x_probe, m_y_probe = \
             cur_params.execute('SELECT xm_probe, ym_probe \
@@ -116,18 +128,18 @@ try:
                 rotate_vector(-s_x_next, -s_y_next, theta_pattern_tilled)
             suss.move_to_xy_from_home(x_next_from_home, y_next_from_home)
             suss.contact()
-            if first_measurement and mesa == c.sw_mesas[0]:
+            if first_measurement and mesa == mesas[0]:
                 input('Contact the prober.')
                 first_measurement = False
-            for V in c.sw_agi_Vs:
+            for V in agi_Vs:
                 t0 = int(time.strftime('%Y%m%d%H%M%S'))  # 20150830203015
-                Vs, Is, aborted = agi.double_sweep_from_zero(2, 1, V, None, c.sw_agi_comp)
+                Vs, Is, aborted = agi.double_sweep_from_zero(2, 1, V, None, agi_comp)
                 points = len(Vs)
                 # XY offset: UPDATE params SET X=X+8, Y=Y+12 WHERE sample="E0339 X9-12 Y13-16" and mesa="D56.3"
                 cur_params.execute('INSERT INTO IV_params(t0,sample,X,Y,mesa,status,measPoints,compliance,voltage,instrument) \
                                     VALUES(?,?,?,?,?,?,?,?,?,?)',
-                                   (t0, c.sw_sample, X, Y, mesa,
-                                    255, points, c.sw_agi_comp, V, 'SUSS PA300 + Agilent 4156C')
+                                   (t0, sample, X, Y, mesa,
+                                    255, points, agi_comp, V, 'SUSS PA300 + Agilent 4156C')
                                   )
                 tmp = zip([t0] * points, Vs, Is)  # [(t0, V0, I0), (t0, V1, I1), ...]
                 cur_IVs.executemany('''INSERT INTO IVs(t0, V, I) VALUES(?, ?, ?)''', tmp)
@@ -138,7 +150,7 @@ try:
                 if aborted:
                     break
             suss.align()
-        suss.separate()
+        #suss.separate()
 
 except:
     if not debug_mode:
